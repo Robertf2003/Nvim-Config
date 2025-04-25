c7ae08d79159c8624df33e65f1e6830445b792f0
Robert <rmyers@AAD-PF50FW2H>
2025-04-25 10:30:32 -0500

    Add nvim-cmp for autocomplete

---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,14 @@
+return {
+  {
+  "hrsh7th/nvim-cmp",
+  lazy = true,
+  dependencies = {
+    "tpope/vim-fugitive",
+    "neovim/nvim-lspconfig",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+  }
+  }
+}
