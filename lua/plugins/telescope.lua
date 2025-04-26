a99437ac05aaf5fd99c22272e124d53c5acde354
Robertf2003 <robertjr.legend.electric@gmail.com>
2025-04-26 18:13:29 -0500

    Fix keymaps for Telescope
    Fix the keymaps for Telescope. Before it had Neogen's keymap and not the
    base keymapping that the Telescope Github page provides.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,10 +4,26 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     keys = {
       {
-        "<leader>ng",
-        "<cmd>Neogen<cr>",
-        desc = "Telescope Live Grep",
+        "<leader>ff",
+        "<cmd>Telescope find_files<cr>",
+        desc = "Use Telescope to find files by name",
       },
+      {
+        "<leader>fg",
+        "<cmd>Telescope live_grep<cr>",
+        desc = "Use Telescope to grep for something in files",
+      },
+      {
+        "<leader>fb",
+        "<cmd>Telescope buffers<cr>",
+        desc = "See Telescope buffers",
+      },
+      {
+        "<leader>fh",
+        "<cmd>Telescope help_tags<cr>",
+        desc = "See Telescope help tages",
+      },
+
     },
   }
 }
