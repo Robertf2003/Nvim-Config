a55b2852780e3f56c6fb0d2347f1847125b329fb
Robert <rmyers@AAD-PF50FW2H>
2025-04-26 17:33:18 -0500

    Update Neogen's python convention

--- lua/plugins/neogen.lua
+++ lua/plugins/neogen.lua
@@ -3,4 +3,14 @@ return {
   config = true,
   -- Uncomment next line if you want to follow only stable versions
   -- version = "*" 
+  opts = {
+    languages = {
+        python = {
+            template = {
+                annotation_convention = "reST"
+            }
+        },
+    }
+
+  }
 }
