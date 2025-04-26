dd195eb5c07afa030d91f1b2bd5ca371ae66547c
Robert <rmyers@AAD-PF50FW2H>
2025-04-26 17:32:32 -0500

    Update treesitter to find languages

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,3 +1,11 @@
 return { 
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+  {
+    "nvim-treesitter/nvim-treesitter", 
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python" },
+      sync_install = true,
+      auto_install = true,
+    },
+  }
 }
