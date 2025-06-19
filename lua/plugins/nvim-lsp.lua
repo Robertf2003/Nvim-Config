1520296a67b5c74e833cc841cf02d670eaaa185f
Robertf2003 <robertjr.legend.electric@gmail.com>
2025-06-18 22:20:03 -0500

    Add LspClangdSwitchSwourceHeader Keybind

--- lua/plugins/nvim-lsp.lua
+++ lua/plugins/nvim-lsp.lua
@@ -1,6 +1,13 @@
 return { 
   {
     "neovim/nvim-lspconfig",
-     build = "npm i -g pyright"
-  }
+    build = "npm i -g pyright",
+    keys = {
+      {
+        "<leader>hh",
+        "<cmd>LspClangdSwitchSourceHeader<cr>",
+        desc = "Switch between header and source file",
+      },
+    }
+  },
 }
