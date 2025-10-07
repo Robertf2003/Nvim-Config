cfdcc77290696f2cf013c2875894ebe9f850e1f5
Robertf2003 <robertjr.legend.electric@gmail.com>
2025-10-07 14:15:08 -0500

    add build steps for lsp

--- lua/plugins/nvim-lsp.lua
+++ lua/plugins/nvim-lsp.lua
@@ -1,13 +1,16 @@
 return { 
   {
     "neovim/nvim-lspconfig",
-    build = "npm i -g pyright",
+    build = "npm i -g pyright && \
+    pip install cmake-language-server && \
+    npm i -g vscode-langservers-extracted && \
+    npm install -g vim-language-server",
     keys = {
       {
         "<leader>hh",
         "<cmd>LspClangdSwitchSourceHeader<cr>",
         desc = "Switch between header and source file",
       },
-    }
-  },
+    },
+  }
 }
