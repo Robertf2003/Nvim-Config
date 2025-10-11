23433e1dc99f8f28cc70a0d7cfe3454bf370badd
Robertf2003 <robertjr.legend.electric@gmail.com>
2025-10-10 22:27:36 -0500

    Change tab convention

--- init.lua
+++ init.lua
@@ -3,8 +3,8 @@ local o = vim.opt
 o.swapfile = false
 -- indentation
 o.expandtab = true   -- Turns tabs into space
-o.shiftwidth = 2     -- shift 4 spaces when tab
-o.tabstop = 2        -- 1 tab == 2 spaces
+o.shiftwidth = 4     -- shift 4 spaces when tab
+o.tabstop = 4        -- 1 tab == 4 spaces
 o.smartindent = true -- auto indent lines
 -- UI
 o.colorcolumn = '100'-- Line at column 100
